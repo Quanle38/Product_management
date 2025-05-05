16b2794859056c22f7e936782b704a314a24ffa6
unknown <halequan3082004@gmail.com>
2025-05-05 07:35:47 +0700

    add product,dashboard controller

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,7 +1,12 @@
-const systemConfig = require("../../config/system")
+const systemConfig = require("../../config/system");
+const  productRoute = require("./product/product.route");
+const dashboardRoute = require('./dashboard/dashboard.route');
 module.exports = (app) =>{
     const PATH_ADMIN = systemConfig.prefixAdmin;
-    app.get(PATH_ADMIN + "/dashboard",(req,res)=>{
-        res.render("admin/pages/dashboard/index")
-    })
+    //dashboard
+    app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
+    
+    //product
+    app.use(`${PATH_ADMIN}/product`, productRoute);
+  
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,7 +2,6 @@ const productRoute = require("./products/product.route")
 const homeRoute = require("./home/home.route") 
 module.exports = (app) => {
     app.use("/",homeRoute)
-
     app.use("/products", productRoute);
 };
 
